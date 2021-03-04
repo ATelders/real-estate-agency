9a7e4aa24ebae86607944dfb322184eb66667ba1
Arthur Telders <atelders@gmail.com>
2021-03-04 23:41:31 +0100

    create py files and screenshot v0

---
+++ CRUD.py
@@ -0,0 +1,138 @@
+from __future__ import print_function
+from datetime import date, datetime, timedelta
+import mysql.connector
+
+cnx = mysql.connector.connect(user='arthur', database='agence_location_python', password='SGBDR')
+cursor = cnx.cursor()
+
+
+add_commune = ("INSERT INTO communes "
+               "(commune, nombre_habitants, distance_agence) "
+               "VALUES (%s, %s, %s)")
+add_type_logement = ("INSERT INTO types_logement "
+              "(type, charges) "
+              "VALUES (%s, %s)")
+add_locataire = ("INSERT INTO locataires "
+              "(nom, prenom, date_de_naissance) "
+              "VALUES (%s, %s, %s)")
+add_telephone = ("INSERT INTO telephones "
+              "(telephone, locataire_id) "
+              "VALUES (%s, %s)")
+add_logement = ("INSERT INTO logements "
+              "(adresse, superficie, loyer, commune_id, type_logement_id) "
+              "VALUES (%s, %s, %s, %s, %s)")
+add_contrat_de_location = ("INSERT INTO types_logement "
+              "(locataire_id, logement_id, date_entree, date_sortie) "
+              "VALUES (%s, %s, %s, %s)")
+
+data_commune = [
+    ['Lille', 232741, 0],
+    ['Paris', 10000000, 200],
+    ['Dunkerque', 10000000, 200],
+    ['Pékin', 10000000, 200],
+    ['Armentières', 10000000, 200],
+]
+
+
+data_type_logement = [
+    ["Studio", 30],
+    ["T2", 40],
+    ["T3", 50],
+    ["T4", 60],
+    ["Maison", 70]
+]
+
+data_locataire = [
+    ["Hallyday", "Johnny", date(1943,1,8)],
+    ["Presley", "Elvis", date(1935,1,8)],
+    ["Zidane", "Zinedine", date(1970,1,8)],
+    ["Sanson", "Véronique", date(1958,1,8)],
+    ["chanteuse", "Rihanna", date(1988,1,8)]
+]
+
+data_telephone = [
+    ["0123456789", 1],
+    ["0223456789", 2],
+    ["0323456789", 3],
+    ["0423456789", 4],
+    ["0523456789", 5],
+]
+
+data_logement = [
+    ["1, rue de l'avenir", 30, 400, 2, "Studio"],
+    ["2, rue de l'avenir", 40, 500, 3, "T4"],
+    ["3, rue de l'avenir", 50, 600, 4, "T3"],
+    ["4, rue de l'avenir", 60, 700, 5, "T2"],
+    ["5, rue de l'avenir", 70, 800, 1, "Maison"]
+]
+
+data_contrat_de_location = [
+    [1, 2, datetime(2002, 12, 4), datetime(2020,2,2)],
+    [2, 3, datetime(2012, 12, 4, 20, 30, 40)],
+    [3, 4, datetime(2018, 12, 4, 20, 30, 40), datetime(2020, 12, 4, 20, 30, 40)],
+    [4, 5, datetime(2020, 12, 4, 20, 30, 40)],
+    [5, 1, datetime(2019, 12, 4, 20, 30, 40)],
+]
+
+
+# Insert commune
+
+for i in range(len(data_commune)) :
+    try :
+        cursor.execute(add_commune, data_commune[i])
+        print("Commune créée : {}".format(data_commune[i][0]))
+    except :
+        print("La commune {} existe déjà".format(data_type_logement[i][0]))
+
+# Insert type de logement
+
+for i in range(len(data_type_logement)) :
+    try :
+        cursor.execute(add_type_logement, data_type_logement[i])
+        print("Type créé : {}".format(data_type_logement[i][0]))
+    except :
+        print("Le type {} existe déjà".format(data_type_logement[i][0]))
+
+# Insert locataire
+
+for i in range(len(data_locataire)) :
+    try :
+        cursor.execute(add_locataire, data_locataire[i])
+        print("Locataire créé : {}".format(data_locataire[i][0]))
+    except :
+        print("Le locataire {} existe déjà".format(data_locataire[i][0]))
+
+# Insert telephone
+
+for i in range(len(data_telephone)) :
+    try :
+        cursor.execute(add_telephone, data_telephone[i])
+        print("Téléphone créé : {}".format(data_telephone[i][0]))
+    except :
+        print("Le téléphone {} existe déjà".format(data_telephone[i][0]))
+
+# Insert logement
+
+for i in range(len(data_logement)) :
+    try :
+        cursor.execute(add_logement, data_logement[i])
+        print("Logement créé : {}".format(data_logement[i][0]))
+    except :
+        print("Le logement {} existe déjà".format(data_logement[i][0]))
+
+# Insert contrat de location
+
+for i in range(len(data_contrat_de_location)) :
+    try :
+        cursor.execute(add_contrat_de_location, data_contrat_de_location[i])
+        print("Contrat créé : {}".format(data_contrat_de_location[i][0]))
+    except :
+        print("Le contrat {} existe déjà".format(data_contrat_de_location[i][0]))
+
+
+
+# Make sure data is committed to the database
+cnx.commit()
+
+cursor.close()
+cnx.close()
---
+++ create_database_tables.py
@@ -0,0 +1,140 @@
+# Inspiré de l'exemple de la documentation mySQL
+# https://dev.mysql.com/doc/connector-python/en/connector-python-example-ddl.html
+
+from __future__ import print_function
+
+import mysql.connector
+from mysql.connector import errorcode
+
+
+# Initialisation du nom de la base de données
+
+DB_NAME = 'agence_location_python'
+
+
+# Initialisation du dictionnaire de tables
+
+TABLES = {}
+
+TABLES['types_logement'] = (
+    "CREATE TABLE `types_logement` ("
+    "   `type` varchar(20) NOT NULL,"
+    "   `charges` decimal(6,2) DEFAULT NULL,"
+    "   PRIMARY KEY (`type`)"
+    ")  ENGINE=InnoDB"
+)
+
+TABLES['communes'] = (
+    "CREATE TABLE `communes` ("
+    "   `id` smallint unsigned NOT NULL AUTO_INCREMENT,"
+    "   `commune` VARCHAR(60) NOT NULL,"
+    "   `nombre_habitants` int unsigned DEFAULT NULL,"
+    "   `distance_agence` decimal(6,2) DEFAULT NULL,"
+    "   PRIMARY KEY (`id`)"
+    ")  ENGINE=InnoDB"
+)
+
+TABLES['logements'] = (
+    "CREATE TABLE `logements` ("
+    "   `id` mediumint unsigned NOT NULL AUTO_INCREMENT,"
+    "   `adresse` varchar(200) NOT NULL,"
+    "   `superficie` decimal(6,2),"
+    "   `loyer` decimal(7,2),"
+    "   `commune_id` smallint unsigned DEFAULT NULL,"
+    "   `type_logement_id` varchar(20) DEFAULT NULL,"
+    "   PRIMARY KEY (`id`),"
+    "   KEY `logement_ibfk_1` (`commune_id`),"
+    "   KEY `logement_ibfk_2` (`type_logement_id`),"
+    "   CONSTRAINT `logement_ibfk_1` FOREIGN KEY (`commune_id`) "
+    "       REFERENCES `communes` (`id`) ON DELETE SET NULL,"
+    "   CONSTRAINT `logement_ibfk_2` FOREIGN KEY (`type_logement_id`) "
+    "       REFERENCES `types_logement` (`type`) ON DELETE SET NULL"
+    ") ENGINE=InnoDB"
+)
+
+TABLES['locataires'] = (
+    "CREATE TABLE `locataires` ("
+    "   `id` mediumint unsigned NOT NULL AUTO_INCREMENT,"
+    "   `nom` varchar(100) DEFAULT NULL,"
+    "   `prenom` varchar(60) DEFAULT NULL,"
+    "   `date_de_naissance` date DEFAULT NULL,"
+    "   PRIMARY KEY (`id`)"
+    ")  ENGINE=InnoDB"
+)
+
+TABLES['telephones'] = (
+    "CREATE TABLE `telephones` ("
+    "   `telephone` varchar(16) NOT NULL,"
+    "   `locataire_id` mediumint unsigned NOT NULL,"
+    "   PRIMARY KEY (`telephone`),"
+    "   KEY `telephone_ibfk_1` (`locataire_id`),"
+    "   CONSTRAINT `telephone_ibfk_1` FOREIGN KEY (`locataire_id`) "
+    "       REFERENCES `locataires` (`id`) ON DELETE CASCADE"
+    ")  ENGINE=InnoDB"
+)
+
+TABLES['contrats_de_location'] = (
+    "CREATE TABLE `contrats_de_location` ("
+    "   `id` mediumint unsigned NOT NULL AUTO_INCREMENT,"
+    "   `locataire_id` mediumint unsigned DEFAULT NULL,"
+    "   `logement_id` mediumint unsigned DEFAULT NULL,"
+    "   `date_entree` datetime DEFAULT NULL,"
+    "   `date_sortie` datetime DEFAULT NULL,"
+    "   PRIMARY KEY (`id`),"
+    "   KEY `contrat_de_location_ibfk_1` (`locataire_id`),"
+    "   KEY `contrat_de_location_ibfk_2` (`logement_id`),"
+    "   CONSTRAINT `contrat_de_location_ibfk_1` FOREIGN KEY (`locataire_id`) "
+    "       REFERENCES `locataires` (`id`) ON DELETE SET NULL,"
+    "   CONSTRAINT `contrat_de_location_ibfk_2` FOREIGN KEY (`logement_id`) "
+    "       REFERENCES `logements` (`id`) ON DELETE SET NULL"
+    ")  ENGINE=InnoDB"
+)
+
+# Connexion à mySQL
+
+cnx = mysql.connector.connect(user='arthur', password="SGBDR")
+cursor = cnx.cursor()
+
+# Fonction de création de la base de données
+
+def create_database(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+
+# Créer la base de données si elle n'existe pas déjà
+
+try:
+    cursor.execute("USE {}".format(DB_NAME))
+except mysql.connector.Error as err:
+    print("Database {} does not exists.".format(DB_NAME))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor)
+        print("Database {} created successfully.".format(DB_NAME))
+        cnx.database = DB_NAME
+    else:
+        print(err)
+        exit(1)
+
+
+# Créer les tables du dictionnaire TABLES
+
+for table_name in TABLES:
+    table_description = TABLES[table_name]
+    try:
+        print("Creating table {}: ".format(table_name), end='')
+        cursor.execute(table_description)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("already exists.")
+        else:
+            print(err.msg)
+    else:
+        print("OK")
+
+cursor.close()
+cnx.close()
