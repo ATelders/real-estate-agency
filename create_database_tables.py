622e425e884670cddb59496579f56ec08f3d7761
Arthur Telders <atelders@gmail.com>
2021-03-07 19:57:40 +0100

    update

--- create_database_tables.py
+++ create_database_tables.py
@@ -1,7 +1,7 @@
 # Inspiré de l'exemple de la documentation mySQL
 # https://dev.mysql.com/doc/connector-python/en/connector-python-example-ddl.html
 
-from __future__ import print_function
+#from __future__ import print_function
 
 import mysql.connector
 from mysql.connector import errorcode
@@ -19,77 +19,90 @@ DB_NAME = 'agence_location_python'
 TABLES = {}
 
 TABLES['types_logement'] = (
-    "CREATE TABLE `types_logement` ("
-    "   `type` varchar(20) NOT NULL,"
-    "   `charges` decimal(6,2) DEFAULT NULL,"
-    "   PRIMARY KEY (`type`)"
-    ")  ENGINE=InnoDB"
+    '''
+    CREATE TABLE types_logement (
+       type varchar(20) NOT NULL,
+       charges decimal(6,2) DEFAULT NULL,
+       PRIMARY KEY (type)
+    )  ENGINE=InnoDB
+    '''
 )
 
 TABLES['communes'] = (
-    "CREATE TABLE `communes` ("
-    "   `id` smallint unsigned NOT NULL AUTO_INCREMENT,"
-    "   `commune` VARCHAR(60) NOT NULL,"
-    "   `nombre_habitants` int unsigned DEFAULT NULL,"
-    "   `distance_agence` decimal(6,2) DEFAULT NULL,"
-    "   PRIMARY KEY (`id`)"
-    ")  ENGINE=InnoDB"
+    '''
+    CREATE TABLE communes (
+        id smallint unsigned NOT NULL AUTO_INCREMENT,
+        commune VARCHAR(60) NOT NULL,
+        nombre_habitants int unsigned DEFAULT NULL,
+        distance_agence decimal(6,2) DEFAULT NULL,
+        PRIMARY KEY (id)
+    )   ENGINE=InnoDB
+    '''
 )
 
 TABLES['logements'] = (
-    "CREATE TABLE `logements` ("
-    "   `id` mediumint unsigned NOT NULL AUTO_INCREMENT,"
-    "   `adresse` varchar(200) NOT NULL,"
-    "   `superficie` decimal(6,2),"
-    "   `loyer` decimal(7,2),"
-    "   `commune_id` smallint unsigned DEFAULT NULL,"
-    "   `type_logement_id` varchar(20) DEFAULT NULL,"
-    "   PRIMARY KEY (`id`),"
-    "   KEY `logement_ibfk_1` (`commune_id`),"
-    "   KEY `logement_ibfk_2` (`type_logement_id`),"
-    "   CONSTRAINT `logement_ibfk_1` FOREIGN KEY (`commune_id`) "
-    "       REFERENCES `communes` (`id`) ON DELETE SET NULL,"
-    "   CONSTRAINT `logement_ibfk_2` FOREIGN KEY (`type_logement_id`) "
-    "       REFERENCES `types_logement` (`type`) ON DELETE SET NULL"
-    ") ENGINE=InnoDB"
+    '''
+    CREATE TABLE logements (
+        id mediumint unsigned NOT NULL AUTO_INCREMENT,
+        adresse varchar(200) NOT NULL,
+        superficie decimal(6,2),
+        loyer decimal(7,2),
+        commune_id smallint unsigned DEFAULT NULL,
+        type_logement_id varchar(20) DEFAULT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (commune_id)
+           REFERENCES communes (id)
+           ON DELETE SET NULL
+           ON UPDATE CASCADE,
+        FOREIGN KEY (type_logement_id)
+           REFERENCES types_logement(type)
+           ON DELETE SET NULL
+           ON UPDATE CASCADE
+    )   ENGINE=InnoDB
+    '''
 )
 
 TABLES['locataires'] = (
-    "CREATE TABLE `locataires` ("
-    "   `id` mediumint unsigned NOT NULL AUTO_INCREMENT,"
-    "   `nom` varchar(100) DEFAULT NULL,"
-    "   `prenom` varchar(60) DEFAULT NULL,"
-    "   `date_de_naissance` date DEFAULT NULL,"
-    "   PRIMARY KEY (`id`)"
-    ")  ENGINE=InnoDB"
+    '''
+    CREATE TABLE locataires (
+       id mediumint unsigned NOT NULL AUTO_INCREMENT,
+       nom varchar(100) DEFAULT NULL,
+       prenom varchar(60) DEFAULT NULL,
+       date_de_naissance date DEFAULT NULL,
+       PRIMARY KEY (id)
+    )  ENGINE=InnoDB
+    '''
 )
 
 TABLES['telephones'] = (
-    "CREATE TABLE `telephones` ("
-    "   `telephone` varchar(16) NOT NULL,"
-    "   `locataire_id` mediumint unsigned NOT NULL,"
-    "   PRIMARY KEY (`telephone`),"
-    "   KEY `telephone_ibfk_1` (`locataire_id`),"
-    "   CONSTRAINT `telephone_ibfk_1` FOREIGN KEY (`locataire_id`) "
-    "       REFERENCES `locataires` (`id`) ON DELETE CASCADE"
-    ")  ENGINE=InnoDB"
+    '''
+    CREATE TABLE telephones (
+        telephone varchar(16) NOT NULL,
+        locataire_id mediumint unsigned NOT NULL,
+        PRIMARY KEY (telephone),
+        FOREIGN KEY (locataire_id)
+            REFERENCES locataires(id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+    )   ENGINE=InnoDB
+    '''
 )
 
 TABLES['contrats_de_location'] = (
-    "CREATE TABLE `contrats_de_location` ("
-    "   `id` mediumint unsigned NOT NULL AUTO_INCREMENT,"
-    "   `locataire_id` mediumint unsigned DEFAULT NULL,"
-    "   `logement_id` mediumint unsigned DEFAULT NULL,"
-    "   `date_entree` datetime DEFAULT NULL,"
-    "   `date_sortie` datetime DEFAULT NULL,"
-    "   PRIMARY KEY (`id`),"
-    "   KEY `contrat_de_location_ibfk_1` (`locataire_id`),"
-    "   KEY `contrat_de_location_ibfk_2` (`logement_id`),"
-    "   CONSTRAINT `contrat_de_location_ibfk_1` FOREIGN KEY (`locataire_id`) "
-    "       REFERENCES `locataires` (`id`) ON DELETE SET NULL,"
-    "   CONSTRAINT `contrat_de_location_ibfk_2` FOREIGN KEY (`logement_id`) "
-    "       REFERENCES `logements` (`id`) ON DELETE SET NULL"
-    ")  ENGINE=InnoDB"
+    '''
+    CREATE TABLE contrats_de_location (
+        id mediumint unsigned NOT NULL AUTO_INCREMENT,
+        locataire_id mediumint unsigned DEFAULT NULL,
+        logement_id mediumint unsigned DEFAULT NULL,
+        date_entree datetime DEFAULT NULL,
+        date_sortie datetime DEFAULT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (locataire_id)
+            REFERENCES locataires (id) ON DELETE SET NULL,
+        FOREIGN KEY (logement_id)
+            REFERENCES logements (id) ON DELETE SET NULL
+    )   ENGINE=InnoDB
+    '''
 )
 
 # Connexion à mySQL
